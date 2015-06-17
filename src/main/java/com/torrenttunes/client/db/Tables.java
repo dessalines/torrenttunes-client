97092be7b14b210637638d40c86df15b2d4a12a5
tyler <happydooby@gmail.com>
2015-06-17 11:44:11 -0400

    forgot to update the fucking table db uses

--- src/main/java/com/torrenttunes/client/db/Tables.java
+++ src/main/java/com/torrenttunes/client/db/Tables.java
@@ -1,36 +1,43 @@
 package com.torrenttunes.client.db;
 
 import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.DbName;
 import org.javalite.activejdbc.annotations.Table;
 
 
 public class Tables {
 
-
+	@DbName("ttc")
 	@Table("settings")
 	public static class Settings extends Model {}
 	public static final Settings SETTINGS = new Settings();
 	
+	@DbName("ttc")
 	@Table("library")
 	public static class Library extends Model {}
 	public static final Library LIBRARY = new Library();
 	
+	@DbName("ttc")
 	@Table("queue_track")
 	public static class QueueTrack extends Model {}
 	public static final QueueTrack QUEUE_TRACK = new QueueTrack();
 	
+	@DbName("ttc")
 	@Table("queue_view")
 	public static class QueueView extends Model {}
 	public static final QueueView QUEUE_VIEW = new QueueView();
 	
+	@DbName("ttc")
 	@Table("playlist_track")
 	public static class PlaylistTrack extends Model {}
 	public static final PlaylistTrack PLAYLIST_TRACK = new PlaylistTrack();
 	
+	@DbName("ttc")
 	@Table("playlist_track_view")
 	public static class PlaylistTrackView extends Model {}
 	public static final PlaylistTrackView PLAYLIST_TRACK_VIEW = new PlaylistTrackView();
 	
+	@DbName("ttc")
 	@Table("playlist")
 	public static class Playlist extends Model {}
 	public static final Playlist PLAYLIST = new Playlist();
