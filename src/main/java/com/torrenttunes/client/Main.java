d878b0fdf855725c6e842ec7670a86bb21494916
tyler <happydooby@gmail.com>
2015-06-16 20:50:11 -0400

    install only flag on auto-updating

--- src/main/java/com/torrenttunes/client/Main.java
+++ src/main/java/com/torrenttunes/client/Main.java
@@ -20,106 +20,108 @@ import static com.torrenttunes.client.db.Tables.*;
 
 
 public class Main {
-	
+
 	static Logger log = (Logger)LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
-	
+
 	@Option(name="-uninstall",usage="Uninstall torrenttunes-client.(WARNING, this deletes your library)")
 	private boolean uninstall;
-	
+
 	@Option(name="-recopy",usage="Recopies your source folders")
 	private boolean recopy;
-	
+
 	@Option(name="-installonly",usage="Only installs it, doesn't run it")
 	private boolean installOnly;
-	
+
 	@Option(name="-loglevel", usage="Sets the log level [INFO, DEBUG, etc.]")     
 	private String loglevel = "INFO";
-	
+
 	@Option(name="-sharedirectory", usage="Scans a directory to share")     
 	private String shareDirectory = null;
-	
-	
+
+
 	public void doMain(String[] args) {
-		
+
 		parseArguments(args);
-		
+
 		// See if the user wants to uninstall it
 		if (uninstall) {
 			Tools.uninstall();
 		}
-		
+
 		log.info("recopy = " + recopy);
 		log.setLevel(Level.toLevel(loglevel));
-		
-		Updater.checkForUpdate();
-		
+
+		if (!installOnly) {
+			Updater.checkForUpdate();
+		}
+
 		// Install Shortcuts
 		Tools.setupDirectories();
-		
+
 		Tools.copyResourcesToHomeDir(recopy);
 
-		
-		
+
+
 		Tools.addExternalWebServiceVarToTools();
 
 		InitializeTables.initializeTables();
-		
+
 		setupSettings();
-		
-		
+
+
 		if (installOnly) {
 			System.exit(0);
 		}
-		
-		
+
+
 		WebService.start();
-		
+
 		Tools.pollAndOpenStartPage();
-		
+
 		if (shareDirectory != null) {
 			ScanDirectory.start(new File(shareDirectory));
 		}
-		
+
 		LibtorrentEngine.INSTANCE.startSeedingLibrary();
-		
-	
-	
-		
+
+
+
+
 	}
 
 	private static void setupSettings() {
 		Tools.dbInit();
 		Settings s = SETTINGS.findFirst("id = ?", 1);
 		Tools.dbClose();
-		
 
-		
+
+
 		setupMusicStoragePath(s);
 		setupLibTorrentSettings(s);
 	}
-	
-	
+
+
 	private static void setupMusicStoragePath(Settings s) {
 		String storagePath = s.getString("storage_path");
 		DataSources.MUSIC_STORAGE_PATH = storagePath;
 		log.info("Storage path = " + DataSources.MUSIC_STORAGE_PATH);
 	}
-	
+
 	private static void setupLibTorrentSettings(Settings s) {
 		LibtorrentEngine lte = LibtorrentEngine.INSTANCE;
 		Integer maxDownloadSpeed = s.getInteger("max_download_speed");
 		Integer maxUploadSpeed = s.getInteger("max_upload_speed");
 		maxDownloadSpeed = (maxDownloadSpeed != -1) ? maxDownloadSpeed : 0;
 		maxUploadSpeed = (maxUploadSpeed != -1) ? maxUploadSpeed : 0;
-//		lte.getSessionSettings().setDownloadRateLimit(1000 * maxDownloadSpeed);
-//		lte.getSessionSettings().setUploadRateLimit(1000 * maxUploadSpeed);
-//		lte.updateSettings();
+		//		lte.getSessionSettings().setDownloadRateLimit(1000 * maxDownloadSpeed);
+		//		lte.getSessionSettings().setUploadRateLimit(1000 * maxUploadSpeed);
+		//		lte.updateSettings();
 	}
-	
-	
-	
-	
-	
+
+
+
+
+
 	private void parseArguments(String[] args) {
 		CmdLineParser parser = new CmdLineParser(this);
 
@@ -146,8 +148,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		new Main().doMain(args);
-	
+
 	}
-	
+
 
 }
