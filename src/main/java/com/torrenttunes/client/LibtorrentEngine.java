6445fe90a4445d71a50b8d43a5dd92d8b74f9394
tyler <happydooby@gmail.com>
2015-06-25 11:52:38 -0400

    again

--- src/main/java/com/torrenttunes/client/LibtorrentEngine.java
+++ src/main/java/com/torrenttunes/client/LibtorrentEngine.java
@@ -76,8 +76,8 @@ public enum LibtorrentEngine  {
 		sessionSettings.allowMultipleConnectionsPerIp(true);
 		sessionSettings.announceDoubleNAT(true);
 		sessionSettings.setUploadRateLimit(0);
-		sessionSettings.setPeerTimeout(15);
-		sessionSettings.setInactivityTimeout(30);
+//		sessionSettings.setPeerTimeout(15);
+//		sessionSettings.setInactivityTimeout(30);
 		sessionSettings.setDownloadRateLimit(0);
 		sessionSettings.setConnectionsLimit(100000);
 		sessionSettings.setHalgOpenLimit(5);
