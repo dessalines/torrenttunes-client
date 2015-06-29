7c21cef551c53e433c2334c3ccae736e96b36f74
tyler <happydooby@gmail.com>
2015-06-29 09:56:11 -0400

    fixing a playlist dropdown issue

--- src/main/resources/web/js/main.js
+++ src/main/resources/web/js/main.js
@@ -421,11 +421,15 @@ function addPlaylistDropdowns() {
     var playlists = JSON.parse(e);
     console.log(playlists);
     // fillMustacheWithJson(playlists, addToPlaylistTemplate, ".add_to_playlist_class");
+    if (playlists.length > 0) {}
     Mustache.parse(addToPlaylistTemplate);
     var rendered = Mustache.render(addToPlaylistTemplate, playlists);
     $(rendered).appendTo(".add_to_playlist_class");
 
     setupAddToPlaylist();
+  } else {
+    $('.add_to_playlist_class').addClass("hide");
+  }
 
   });
 
